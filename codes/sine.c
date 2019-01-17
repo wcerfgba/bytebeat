c47c19383db31d34731bd605be85af90a78ba245
John Preston <wcerfgba@riseup.net>
2019-01-17 20:07:46 +0000

    Sine is taking shape but not quite there

--- codes/sine.c
+++ codes/sine.c
@@ -1,7 +1,27 @@
 #include <math.h>
 
+float sawtooth(float x) {
+  return fmod(x, 1);
+}
+
+float triangle(float x) {
+  return 2.0 * fabs(sawtooth(x) - 0.5);
+}
+
+float sine(float x) {
+  return 0.5 * (1.0 + cos(2.0 * 3.14159 * sawtooth(x)));
+}
+
+int unit2byte(float x) {
+  return floor(256.0 * x);
+}
+
+float byte2unit(int x) {
+  return x / 256.0;
+}
+
 int out(int t) {
-  return floor(256.0 * 0.5 * (1.0 + cos( 2.0 * 3.14159 * abs( ((t % 256) / 256.0) ) ) ) );
+  return unit2byte(sine(byte2unit(t)));
 }
 
 int main() {
